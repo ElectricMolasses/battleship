f51484ff130570c0070f97cb5958ef1189410050
Robert Beckett <robert.beckett@onion.red>
2019-10-17 01:23:26 +0000

    Created test function for board creation.

--- test/battleShipTest.js
+++ test/battleShipTest.js
@@ -3,4 +3,7 @@ const assert = chai.assert;
 
 const main = require('../src/battleship');
 
-it('createBoard should create a 10 x 10 array.');
+it('createBoard should have an array of length 10, containing 10 objects', function() {
+  const board = main.createBoard();
+  assert(board.length === 10 && board[9].length === 10);
+});
