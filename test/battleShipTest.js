cd4fc964b5087a5b45528bd947d7f67380843f7c
Robert Beckett <robert.beckett@onion.red>
2019-10-17 19:38:37 +0000

    Implemented test for placing an already placed ship.

--- test/battleShipTest.js
+++ test/battleShipTest.js
@@ -31,7 +31,11 @@ it('placeShip should return true if the ship placement is valid and ship is not 
   assert.isTrue(placeShip(2, 4, 'cruiser', 'horizontal', board, 'player'));
 });
 
-it('placeShip should return false if the ship has already been placed');
+it('placeShip should return false if the ship has already been placed', function() {
+  const board = createBoard();
+  placeShip(2, 4, 'cruiser', 'horizontal', board, 'player');
+  assert.isFalse(placeShip(1, 4, 'cruiser', 'horizontal', board, 'player'));
+});
 
 it('placeShip should return false for out of bounds index, if the coordinates are not within board restrictions', function() {
   const board = createBoard();
