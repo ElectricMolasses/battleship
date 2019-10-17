62e2d4cc383f8a4fafba49d240e4632758315333
Robert Beckett <robert.beckett@onion.red>
2019-10-17 02:26:47 +0000

    Added base parameters to cells that must always be present.

--- src/battleship.js
+++ src/battleship.js
@@ -4,7 +4,10 @@ const createBoard = function() {
   for (let i = 0; i < 10; i++) {
     board.push([]);
     for (let j = 0; j < 10; j++) {
-      board[i].push({});
+      board[i].push({
+        shipType: null,
+        wasShot: false
+      });
     }
   }
 
