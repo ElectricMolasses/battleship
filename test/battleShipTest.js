0735998f5c1fdb6608c558f37c473a10259b0e53
Robert Beckett <robert.beckett@onion.red>
2019-10-17 02:16:26 +0000

    Created pending tests for placeShip function.

--- test/battleShipTest.js
+++ test/battleShipTest.js
@@ -7,3 +7,15 @@ it('createBoard should have an array of length 10, containing 10 objects', funct
   const board = main.createBoard();
   assert(board.length === 10 && board[9].length === 10);
 });
+
+it('placeShip should return true if the ship placement is valid and ship is not already placed');
+
+it('placeShip should return false if the ship has already been places');
+
+it('placeShip should throw an error for out of bounds index, if the coordinates are not within board restrictions');
+
+it('placeShip should return false if part of the ship would be off the grid');
+
+it('placeShip should return false if any part of the ship overlaps with another ship');
+
+it('doesHit should return true when firing on a cell occupied by a ship.');
