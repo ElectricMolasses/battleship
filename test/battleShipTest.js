e0e6be75501d859466d508b5ede613f6d9c72b30
Robert Beckett <robert.beckett@onion.red>
2019-10-17 20:51:00 +0000

    Changed test functions to arrow syntax.

--- test/battleShipTest.js
+++ test/battleShipTest.js
@@ -8,62 +8,62 @@ const createBoard = main.createBoard;
 const placeShip = main.placeShip;
 const removeShip = main.removeShip;
 
-it('SHIPS should contain playerX/Y and computerX/Y as null from launch', function() {
+it('SHIPS should contain playerX/Y and computerX/Y as null from launch', () => {
   assert.isNull(SHIPS.cruiser.player.X);
 });
 
-it('createBoard should have an array of length 10, containing 10 objects', function() {
+it('createBoard should have an array of length 10, containing 10 objects', () => {
   const board = createBoard();
   assert(board.length === 10 && board[9].length === 10);
 });
 
-it('createBoard cells should contain the property shipType', function() {
+it('createBoard cells should contain the property shipType', () => {
   const board = createBoard();
   assert(board[4][8].shipType !== undefined);
 });
 
-it('createBoard cells should instantiate with wasShot = false', function() {
+it('createBoard cells should instantiate with wasShot = false', () => {
   const board = createBoard();
   assert.isFalse(board[1][6].wasShot);
 });
 
-it('placeShip should return true if the ship placement is valid and ship is not already placed', function() {
+it('placeShip should return true if the ship placement is valid and ship is not already placed', () => {
   const board = createBoard();
   assert.isTrue(placeShip(2, 4, 'cruiser', 'horizontal', board, 'player'));
 });
 
-it('placeShip should return false if the ship has already been placed', function() {
+it('placeShip should return false if the ship has already been placed', () => {
   const board = createBoard();
   placeShip(2, 4, 'cruiser', 'horizontal', board, 'player');
   assert.isFalse(placeShip(1, 4, 'cruiser', 'horizontal', board, 'player'));
 });
 
-it('placeShip should return false for out of bounds index, if the coordinates are not within board restrictions', function() {
+it('placeShip should return false for out of bounds index, if the coordinates are not within board restrictions', () => {
   const board = createBoard();
 
   assert.isFalse(placeShip(1, 22, 'destroyer', 'horizontal', board, 'player'));
 });
 
-it('placeShip should return false if part of the ship would be off the grid horizontally', function() {
+it('placeShip should return false if part of the ship would be off the grid horizontally', () => {
   const board = createBoard();
 
   assert.isFalse(placeShip(6, 6, 'carrier', 'horizontal', board, 'player'));
 });
 
-it('placeShip should return false if part of the ship would be off the grid vertically', function() {
+it('placeShip should return false if part of the ship would be off the grid vertically', () => {
   const board = createBoard();
 
   assert.isFalse(placeShip(8, 7, 'carrier', 'vertical', board, 'player'));
 });
 
-it('placeShip should return false if any part of the ship overlaps with another ship', function() {
+it('placeShip should return false if any part of the ship overlaps with another ship', () => {
   const board = createBoard();
 
   placeShip(3, 3, 'carrier', 'vertical', board, 'player');
   assert.isFalse(placeShip(2, 3, 'destroyer', 'horizontal', board, 'player'));
 });
 
-it('placeShip should throw an error if the player argument is not "player" or "computer"', function() {
+it('placeShip should throw an error if the player argument is not "player" or "computer"', () => {
   const board = createBoard();
 
   expect(placeShip.bind(1, 1, 'carrier', 'vertical', board, 'bob')).to.throw(Error);
@@ -85,6 +85,6 @@ it('doesHit should return true when firing on a cell occupied by a ship.');
 
 it('doesHit should return false when firing on a cell that is not occupied by a ship.');
 
-it('doesHit should throw an Error when targeting an out of bounds cell');
+it('doesHit should throw false when targeting an out of bounds cell');
 
-it('doesHit should throw an error when attempting to fire on a cell already fired on');
+it('doesHit should throw false when attempting to fire on a cell already fired on');
