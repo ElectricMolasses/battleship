a91379dd60c60524462327e5847038b4ae534ed6
Robert Beckett <robert.beckett@onion.red>
2019-10-17 01:23:50 +0000

    Implemented and tested createBoard

--- src/battleship.js
+++ src/battleship.js
@@ -1,3 +1,16 @@
 const createBoard = function() {
+  let board = [];
 
+  for (let i = 0; i < 10; i++) {
+    board.push([]);
+    for (let j = 0; j < 10; j++) {
+      board[i].push({});
+    }
+  }
+
+  return board;
+};
+
+module.exports = {
+  createBoard
 };
