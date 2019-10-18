8d31dbf2f4406fe4933510fe4a81ff72dc8f8ec4
Robert Beckett <robert.beckett@onion.red>
2019-10-18 18:51:26 +0000

    added comments regarding sinon usage on console.log tests.

--- test/battleShipTest.js
+++ test/battleShipTest.js
@@ -140,6 +140,12 @@ describe('Main Functions', () => {
       let log = console.log;
       // Sinon automatically cleans up stubs for us before it completes
       // a test, so we shouldn't have to worry about restore().
+
+      // I created a "fake" function that still runs the console.log
+      // command via console.log.apply, but lets sinon spy on it for
+      // testing.  We then confirm in the tests that th arguement passed
+      // to it was the correct string, and that the call actually
+      // completed with calledOnce.
       this.sinon.stub(console, 'log').callsFake(() => {
         return log.apply(log, arguments);
       });
