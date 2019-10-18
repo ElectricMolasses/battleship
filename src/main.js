6e6db13348158adeb0d5e9e99238b171b01baf4e
Robert Beckett <robert.beckett@onion.red>
2019-10-18 22:41:49 +0000

    Implemented drawGrid functions and applied CSS changes to accommodate cell orientation/lines.

--- src/main.js
+++ src/main.js
@@ -1,9 +1,28 @@
 const drawGrid = function(element) {
   const gridWidth = element.innerWidth();
   const gridHeight = element.innerHeight();
-  
 
+  const cellRows = [];
+  const cells = [];
+  
   console.log(`Width: ${gridWidth} Height: ${gridHeight}`);
+  
+  for (let i = 0; i < 10; i++) {
+    cells.push([]);
+    cellRows.push($('<div></div>').width(gridWidth)
+      .height(gridHeight / 10)
+      .addClass('rowContainer')
+      .attr('id', `gridRow${i}`));
+    element.append(cellRows[i]);
+
+    for (let j = 0; j < 10; j++) {
+      cells[i].push($('<div></div>').width(gridWidth / 10)
+        .height(gridHeight / 10)
+        .addClass('cell')
+        .attr('id', `cell${j}-${i}`));
+      cellRows[i].append(cells[i][j]);
+    }
+  }
 };
 
 const drawCell = function() {
