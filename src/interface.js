858a6cdd07ddb6d009782cad4ba4963e57b4e08e
Robert Beckett <robert.beckett@onion.red>
2019-10-19 04:15:12 +0000

    Outlined main approach for interface.js

---
+++ src/interface.js
@@ -0,0 +1,8 @@
+// Include for main.js, will handle communication back and forth
+// between it and the battleship file.
+// Effectively exists to help control what functions main can access,
+// in addition to filtering input.
+
+const request = function(command) {
+
+};
