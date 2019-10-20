a8c840b29d548d7a35436b696678070544efdd07
Robert Beckett <robert.beckett@onion.red>
2019-10-20 01:39:32 +0000

    Removed comments that are no longer relevant, functionality was implemented elsewhere.

--- src/main.js
+++ src/main.js
@@ -162,13 +162,6 @@ const drawMiss = function(cell) {
 const createRemoveListeners = function() {
   for (const ship in SHIP_IMAGES) {
     $(`#remove${ship}`).click(() => {
-      // Check to see if the ship has been placed.
-      // If it has not been placed
-      // Set it to the active ship to place.
-      
-
-      // If it has been placed.
-      // Request to game logic to remove ship.
       eraseShip(ship);
     });
   }
