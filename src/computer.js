eb884629627ab421f89ba04c01cb07dd85f0cff8
Robert Beckett <robert.beckett@onion.red>
2019-11-01 21:27:54 +0000

    Started second level of AI difficulty, now handles two phases, random shots with no hits, and firing around a confirmed hit to attempt to find ship direction.  Cannot yet act on a ships (potential) direction.

--- src/computer.js
+++ src/computer.js
@@ -27,3 +27,28 @@ const dumbShot = function(board, player) {
   } while (board[randomX][randomY].wasShot);
   return [randomX, randomY, fire(randomX, randomY, board, player)];
 };
+
+const okayShot = function(board, player, memory) {
+  let shotX;
+  let shotY;
+
+  if (memory.length < 1) {
+    [shotX, shotY] = dumbShot(board, player);
+  } else if (memory.length === 1) {
+    let firstHit = [memory[0][0], memory[0][1]];
+    let possibleHits = [[firstHit[0] - 1, firstHit[1]],
+      [firstHit[0] + 1, firstHit[1]],
+      [firstHit[0], firstHit[1] - 1],
+      [firstHit[0], firstHit[1] + 1]];
+    
+    do {
+      [shotX, shotY] = Math.floor(Math.random() * possibleHits.length);
+    } while (board[shotX][shotY].wasShot ||
+      shotX > 9 || shotX < 0 ||
+      shotY > 9 || shotY < 0);
+  } else {
+
+  }
+
+  return [shotX, shotY, fire(shotX, shotY, board, player)];
+};
