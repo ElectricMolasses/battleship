d984cfb71af366f4ccc14a484c6a3b9847d5a932
Robert Beckett <robert.beckett@onion.red>
2019-11-04 01:17:04 +0000

    Taking boxed in approach to AI shot choice.  Added loop to build box of current shots on still alive ships and skeleton'd verify line functions.

--- src/computer.js
+++ src/computer.js
@@ -35,6 +35,7 @@ const okayShot = function(board, player, memory) {
 
   if (memory.length < 1) {
     [shotX, shotY] = dumbShot(board, player);
+    memory.push(shotX, shotY);
   } else if (memory.length === 1) {
     let firstHit = [memory[0][0], memory[0][1]];
     let possibleHits = [[firstHit[0] - 1, firstHit[1]],
@@ -47,29 +48,30 @@ const okayShot = function(board, player, memory) {
       shotX > 9 || shotX < 0 ||
       shotY > 9 || shotY < 0);
 
-    result = fire(shotX, shotY, board, player)
+    result = fire(shotX, shotY, board, player);
     if (result) {
       memory.push([shotX, shotY]);
     }
   } else {
-    // Hm.
-    /*
-      Three shots is guaranteed to determine the direction of an already hit ship, whether or not they land.  At this point, we will have at least two confirmed shots anyway, the fourth hit being a guarantee does not matter.
+    let lowX, highX = memory[0][0];
+    let lowY, highY = memory[0][1];
 
-      Once we have two shots, we need a way to deal with the second shot and on potentially landing on different ships.
-      Avoid passing the ship that was hit to the AI if possible.  This isn't functionally neccesary, but it's a better problem to solve.
-
-      Memory will hold shots that have landed on a ship that has not yet sunk.
-      We can clear out shots on a ship that we know we've sunk.
-
-      They player gets information on the type of ship they have sunk, so the AI should be allowed to have this information too.
-
-      The function returns the result of it firing so the player can mark a miss or hit on their board, but we can call it before the return and store that result in a variable, handle it, THEN return the result to the player.
-      ::Refactored below to show such.
-
-      When the computer is told whether or not a ship was sunk, and which ship, the only "effective" information it receives is the length of the ship it sunk.  It needs to determine which set of cells were actually sunk, as WELL as which ones are overlap of other ships and need to stay.  There are conditions where this is IMPOSSIBLE.  How do I work around that restriction?  We'd need to cache certain remembered cells as possibly being overlap, an remove them as we sink other ships.
-    */
+    // This will give us a box that contains all current pending hits.
+    for (const point in memory) {
+      if (point[0] < lowX) lowX = point[0];
+      if (point[0] > highX) highX = point[0];
+      if (point[1] < lowY) lowY = point[1];
+      if (point[1] > highY) highY = point[1];
+    }
   }
+
+  const verifyVerticalLine = function(x1, x2, y) {
+
+  };
+
+  const verifyHorizontalLine = function(x, y1, y2) {
+
+  };
 
   return [shotX, shotY, result];
 };
